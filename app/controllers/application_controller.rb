8a1e643c7774422df8762bb0f6ff85365f47a91a
Suzuka Inomata <suzuka81013@gmail.com>
2022-01-28 16:34:40 +0900

    スマホ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,19 @@ class ApplicationController < ActionController::Base
   end
 
 end
+
+class ApplicationController < ActionController::Base
+  before_action :set_request_variant
+
+  private
+    def set_request_variant
+      case request.user_agent
+      when /iPad/
+        request.variant = :phone
+      when /iPhone/
+        request.variant = :phone
+      when /Android/
+        request.variant = :phone
+      end
+  end
+end
